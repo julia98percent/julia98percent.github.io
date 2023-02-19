abad9e168053089011ed6f7989528bc4a08163b1
Jiwon Chae <47624978+julia98percent@users.noreply.github.com>
2023-02-19 19:06:19 +0900

    [0204-2346] 블로그 타이틀 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,11 +3,13 @@ import * as styles from "./Header.module.scss";
 import { Link } from "gatsby";
 import { menuList } from "../../constants/IndexPage";
 
+const BLOG_NAME = "<PeachJam />";
+
 const Header = () => {
   return (
     <div className={styles.menuWrapper}>
       <div className={styles.left}>
-        <h1>Peach Jam</h1>
+        <h1>{BLOG_NAME}</h1>
       </div>
       <div className={styles.right}>
         {menuList.map(({ name, uri }) => (
