bcc3909be33736660a4554659b1b0fc0d221dcea
Jiwon Chae <47624978+julia98percent@users.noreply.github.com>
2023-02-21 21:19:04 +0900

    [0219-2206] Layout 적용

--- src/pages/allWritings.tsx
+++ src/pages/allWritings.tsx
@@ -1,41 +1,43 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
 import Thumbnail from "../templates/Thumbnail";
-import Header from "../components/Header";
+
 import * as styles from "./index.module.scss";
 import Peach from "../../writing-thumbnail-image/peach.png";
+import Layout from "../components/Layout";
 
 const allWritings = ({ data }: any) => {
   const recentWritings = data.allWritings.nodes;
   return (
-    <main className={styles.wrapper}>
-      <Header />
-      <title>Home Page</title>
-      <div>
-        <p className={styles.recentText}>모든 글</p>
-        {recentWritings.map((item: { frontmatter: any }, index: string) => {
-          const { featuredImage } = item.frontmatter;
+    <Layout>
+      <main className={styles.wrapper}>
+        <title>Home Page</title>
+        <div>
+          <p className={styles.recentText}>모든 글</p>
+          {recentWritings.map((item: { frontmatter: any }, index: string) => {
+            const { featuredImage } = item.frontmatter;
 
-          return (
-            <div className={styles.thumbnailWrapper}>
-              <Link to={`/writings/${item.frontmatter.slug}`} key={index}>
-                <Thumbnail
-                  title={item.frontmatter.title}
-                  entry={`${item.frontmatter.title}은 엄청나게 중요합니당`}
-                  createdAt={item.frontmatter.date.slice(0, 10)}
-                  tags={item.frontmatter.tags}
-                  avatar={
-                    featuredImage
-                      ? featuredImage?.childImageSharp.gatsbyImageData
-                      : Peach
-                  }
-                />
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-    </main>
+            return (
+              <div className={styles.thumbnailWrapper}>
+                <Link to={`/writings/${item.frontmatter.slug}`} key={index}>
+                  <Thumbnail
+                    title={item.frontmatter.title}
+                    entry={`${item.frontmatter.title}은 엄청나게 중요합니당`}
+                    createdAt={item.frontmatter.date.slice(0, 10)}
+                    tags={item.frontmatter.tags}
+                    avatar={
+                      featuredImage
+                        ? featuredImage?.childImageSharp.gatsbyImageData
+                        : Peach
+                    }
+                  />
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+    </Layout>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
 import Thumbnail from "../templates/Thumbnail";
-import Header from "../components/Header";
+import Layout from "../components/Layout/Layout";
 import * as styles from "./index.module.scss";
 import Peach from "../../writing-thumbnail-image/peach.png";
 
@@ -9,35 +9,36 @@ const Index = ({ data }: any) => {
   const recentWritings = data.recentWritings.nodes;
 
   return (
-    <main className={styles.wrapper}>
-      <Header />
-      <title>Home Page</title>
-      <div>
-        <p className={styles.recentText}>최근 글</p>
-        {recentWritings.map((item: { frontmatter: any }, index: string) => {
-          const { featuredImage } = item.frontmatter;
+    <Layout>
+      <main className={styles.wrapper}>
+        <title>Home Page</title>
+        <div>
+          <p className={styles.recentText}>최근 글</p>
+          {recentWritings.map((item: { frontmatter: any }, index: string) => {
+            const { featuredImage } = item.frontmatter;
 
-          return (
-            <div className={styles.thumbnailWrapper}>
-              <Link to={`/writings/${item.frontmatter.slug}`} key={index}>
-                <Thumbnail
-                  title={item.frontmatter.title}
-                  entry={`${item.frontmatter.title}은 엄청나게 중요합니당`}
-                  createdAt={item.frontmatter.date.slice(0, 10)}
-                  tags={item.frontmatter.tags}
-                  avatar={
-                    featuredImage
-                      ? featuredImage?.childImageSharp.gatsbyImageData
-                      : Peach
-                  }
-                />
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-      {/* TODO 검색 버튼 */}
-    </main>
+            return (
+              <div className={styles.thumbnailWrapper}>
+                <Link to={`/writings/${item.frontmatter.slug}`} key={index}>
+                  <Thumbnail
+                    title={item.frontmatter.title}
+                    entry={`${item.frontmatter.title}은 엄청나게 중요합니당`}
+                    createdAt={item.frontmatter.date.slice(0, 10)}
+                    tags={item.frontmatter.tags}
+                    avatar={
+                      featuredImage
+                        ? featuredImage?.childImageSharp.gatsbyImageData
+                        : Peach
+                    }
+                  />
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+        {/* TODO 검색 버튼 */}
+      </main>
+    </Layout>
   );
 };
 
